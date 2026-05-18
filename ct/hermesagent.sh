828657e8bd147d7a6a0b46d603310cf6863182d0
Stephen Chin <steveonjava@gmail.com>
2026-05-18 00:31:19 -0700

    hermesagent.sh: update script now runs as 'hermes' user to prevent root-owned gateway job restarts and avoid permission issues. This eliminates the need for post-update permission fixes.

--- ct/hermesagent.sh
+++ ct/hermesagent.sh
@@ -45,11 +45,10 @@ function update_script() {
   msg_ok "Stopped Services"
 
   msg_info "Updating Hermes Agent"
-  $STD setsid --wait bash -c '
+  $STD setsid --wait su - hermes -c '
     set -a; source /etc/default/hermes; set +a
     /home/hermes/.local/bin/hermes update --yes
   '
-  chown -R hermes:hermes /home/hermes
   msg_ok "Updated Hermes Agent"
 
   msg_info "Starting Services"
